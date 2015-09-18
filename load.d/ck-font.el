dba37e496c8f2ac87940bf246989d1ee9488bc49
Christian Kellner <christian@kellner.me>
2015-09-18 10:30:54 +0200

    [font] add font testing and selection (via plugin)

---
+++ load.d/ck-font.el
@@ -0,0 +1,22 @@
+
+;; set font
+
+(setq ck-fonts
+      '(("Source Code Pro" 12)
+	("Inconsolata" 12)
+	("Menlo" 12)))
+
+(defun font-existsp (name size)
+  (cond ((find-font (font-spec :name (concat name " for Powerline")))
+	 (format "%s for Powerline-%d" name size))
+	((find-font (font-spec :name name))
+	 (format "%s-%d" name size))))
+
+(defun ck-first-font (lst)
+  (or (apply 'font-existsp (car lst))
+      (ck-first-font (cdr lst))))
+
+(let ((foo-font (ck-first-font ck-fonts)))
+  (set-face-attribute 'default nil :font foo-font)
+  (message (concat "Setting font to: " foo-font))
+  )
