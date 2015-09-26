5f0e7a6b97df523cda53bd2374f15818c5e5ac13
Christian Kellner <christian@kellner.me>
2015-09-26 12:43:25 +0200

    [init] move tabbar related stuff together

--- init.el
+++ init.el
@@ -65,6 +65,22 @@
   :config
   (tabbar-mode))
 
+;; == group tabs ==
+(defun tabbar-buffer-groups ()
+  "Return the list of group names the current buffer belongs to.
+This function is a custom function for tabbar-mode's tabbar-buffer-groups.
+This function group all buffers into 3 groups:
+Those Dired, those user buffer, and those emacs buffer.
+Emacs buffer are those starting with “*”."
+  (list
+   (cond
+    ((string-equal "*" (substring (buffer-name) 0 1))
+     "Emacs Buffer")
+    ((eq major-mode 'dired-mode)
+     "Dired")
+    (t "User Buffer" )
+    )))
+
 ; === ido ====
 (ido-mode)
 ;; prevent ido to globally search for files automatically
@@ -115,23 +131,6 @@
   (expand-file-name
    (concat backups-dir "."  (file-name-nondirectory filename) "~")
    (file-name-directory filename)))
-
-
-;; == group tabs ==
-(defun tabbar-buffer-groups ()
-  "Return the list of group names the current buffer belongs to.
-This function is a custom function for tabbar-mode's tabbar-buffer-groups.
-This function group all buffers into 3 groups:
-Those Dired, those user buffer, and those emacs buffer.
-Emacs buffer are those starting with “*”."
-  (list
-   (cond
-    ((string-equal "*" (substring (buffer-name) 0 1))
-     "Emacs Buffer")
-    ((eq major-mode 'dired-mode)
-     "Dired")
-    (t "User Buffer" )
-    )))
 
 ; == recent files ==
 (recentf-mode 1)
