91cba5d253580408b9d0bea9aa33beb84b52d7e7
Christian Kellner <christian@kellner.me>
2016-05-12 00:45:03 +0200

    [init, lang] c: move c family setup to lang-c.el

--- init.el
+++ init.el
@@ -164,58 +164,6 @@
 (add-to-list 'auto-mode-alist
              '("\\.[fF]\\(03\\|08\\)\\'" . f90-mode))
 
-; C
-(setq c-hungry-delete-key t)
-
-(add-hook 'c-mode-hook
-	  (lambda ()
-	    (font-lock-add-keywords nil
-				    '(("\\<\\(FIXME\\):" 1 font-lock-warning-face t)))))
-
-; spaces, not tabs
-(add-hook 'c-mode-hook
-	  (lambda ()
-	    (setq indent-tabs-mode nil)
-	    (setq c-indent-level 4)))
-
-(add-hook 'objc-mode-hook
-	  (lambda ()
-	    (setq indent-tabs-mode nil)
-	    (setq c-indent-level 4)))
-
-(add-hook 'c++-mode-hook
-	  (lambda ()
-	    (setq indent-tabs-mode nil)
-	    (setq c-indent-level 4)))
-
-; objc mode for header files
-(add-to-list 'magic-mode-alist
-	     `(,(lambda ()
-		  (and (string= (file-name-extension (or buffer-file-name "DEF-NAME")) "h")
-		       (re-search-forward "@\\<interface\\>"
-					  magic-mode-regexp-match-limit t)))
-	       . objc-mode))
-
-; switch between header and implementation
-(add-hook 'c-mode-common-hook
-         (lambda ()
-           (define-key c-mode-base-map (kbd "C-c o") 'ff-find-other-file)))
-
-(defconst cc-style-nix
-  '("cc-mode"
-    (c-offsets-alist . ((innamespace . [0])))))
-
-(c-add-style "cc-style-nix" cc-style-nix)
-(setq safe-local-variable-values
-    '((c-set-style . "cc-style-nix")
-     (c-offsets-alist
-      (quote innamespace)
-      [0])))
-
-;; == CUDA mode ==
-(use-package cuda-mode
-  :mode "\\.cu\\'")
-
 ; Tex Mode
 (setq tex-command "xelatex")
 
