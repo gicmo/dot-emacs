41536542a7e7d3e11999556dabcb67d7bf49499b
Christian Kellner <christian@kellner.me>
2016-05-14 11:38:22 +0200

    install_irony.sh: re-work, use system libclang
    And use already downloaded irony server source in elpa package.

--- install_irony.sh
+++ install_irony.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
 
-if [ ! -e irony-mode ]; then
-    git clone --recursive git://github.com/Sarcasm/irony-mode.git
+ironypkg=(elpa/irony-*)
+ironysrc="${ironypkg}/server"
+builddir=irony/build
+target=irony
+binary=irony/bin/irony-server
+
+echo "server source: ${ironysrc}"
+
+if [ ! -e irony/include ]; then
+    echo "downloading clang headers [svn]"
+    svn export http://llvm.org/svn/llvm-project/cfe/trunk/include/clang-c/ irony/include/clang-c
 fi
 
-cd irony-mode
-mkdir -p build
-cd build
-cmake .. && make -j 4
-make install
+rm -rf "$builddir" && mkdir -p "$builddir"
+
+libclang=`xcode-select --print-path`/Toolchains/XcodeDefault.xctoolchain/usr/lib/libclang.dylib
+
+echo "libclang @ $libclang"
+
+cmake -DCMAKE_INSTALL_PREFIX="$target" \
+      -DLIBCLANG_INCLUDE_DIR="irony/include" \
+      -DLIBCLANG_LIBRARY="$libclang" \
+      -H"$ironysrc" -B"$builddir" \
+    && cmake --build "$builddir" --use-stderr --config Release --target install && \
+    install_name_tool -change @rpath/libclang.dylib "$libclang" "$binary" && \
+    "$binary" -v
+
