19e9c118596a300773a6240010908f31ada4adeb
Christian Kellner <christian@kellner.me>
2016-05-15 14:00:28 +0200

    [fonts] move back into 'init.el'

--- init.el
+++ init.el
@@ -332,6 +332,32 @@
   :bind ("C-c s l" . synosaurus-lookup)
   :config (setq synosaurus-backend 'synosaurus-backend-wordnet))
 
+;; -=[ UI
+
+;; -=[ fonts
+
+(setq ck-fonts
+      '(("Hasklig" 12)
+	("Source Code Pro" 12)
+	("Inconsolata" 12)
+	("Menlo" 12)))
+
+(defun font-existsp (name size)
+  (cond ((find-font (font-spec :name (concat name " for Powerline")))
+	 (format "%s for Powerline-%d" name size))
+	((find-font (font-spec :name name))
+	 (format "%s-%d" name size))))
+
+(defun ck-first-font (lst)
+  (or (apply 'font-existsp (car lst))
+      (ck-first-font (cdr lst))))
+
+(when (display-graphic-p)
+ (let ((foo-font (ck-first-font ck-fonts)))
+  (set-face-attribute 'default nil :font foo-font)
+  (message (concat "Setting font to: " foo-font))
+  ))
+
 ;; -=[ color themes
 
 (use-package flatland-theme
