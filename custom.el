d12ce6f2c6467745bddd3ae59fccf2770dd1def3
Christian Kellner <christian@kellner.me>
2016-05-16 16:14:37 +0200

    [custom] cmake-ide-build-dir "build" is safe

--- custom.el
+++ custom.el
@@ -1,3 +1,5 @@
+(custom-set-faces
+ '(git-timemachine-minibuffer-detail-face ((t (:foreground "deep sky blue")))))
 (custom-set-variables
- (custom-set-faces
-  '(git-timemachine-minibuffer-detail-face ((t (:foreground "deep sky blue"))))))
+ '(safe-local-variable-values (quote ((cmake-ide-build-dir . "build")))))
+
