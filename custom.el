e4665ba238cf17f9c99a188edf052c2865076d97
Christian Kellner <christian@kellner.me>
2017-02-04 15:27:10 +0100

    [custom] remove 'custom.el' file
    There was not much in it anyway. All global stuff should go into
    init.el anyway.

--- custom.el
+++
@@ -1,5 +0,0 @@
-(custom-set-faces
- '(git-timemachine-minibuffer-detail-face ((t (:foreground "deep sky blue")))))
-(custom-set-variables
- '(safe-local-variable-values (quote ((cmake-ide-build-dir . "build")))))
-
